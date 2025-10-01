4a35667aae1be1f96b9575f248ba9ca98966e6d1
Giu-lis <giulia.domingues1a@gmail.com>
2025-10-01 09:19:41 -0300

    Add files via upload

--- calculadora/src/main/java/com/devsenai1A/calculadora/controllers/CalculadoraController.java
+++ calculadora/src/main/java/com/devsenai1A/calculadora/controllers/CalculadoraController.java
@@ -1,7 +1,12 @@
 package com.devsenai1A.calculadora.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,21 +17,36 @@ public class CalculadoraController {
         return a + b;
     }
 	
-    @GetMapping("/subtrair")
-    public double subtrair(@RequestParam double a, @RequestParam double b) {
-        return a - b;
-    }
-
-    @GetMapping("/multiplicar")
-    public double multiplicar(@RequestParam double a, @RequestParam double b) {
-        return a * b;
-    }
-
-    @GetMapping("/dividir")
-    public double dividir(@RequestParam double a, @RequestParam double b) {
-        if (b == 0) {
-            throw new IllegalArgumentException("Divisão por zero não é permitida.");
-        }
-        return a / b;
-    }
+	//colocar os métodos subtrair, multiplicar e dividir
+	
+	@PostMapping("/calcular")
+	@ResponseBody
+    public Map<String, Object> calcularJson(
+    		@RequestParam double num1,
+    		@RequestParam double num2,
+    		@RequestParam String operacao){
+		
+		double resultado =0;
+		String erro = null;
+		
+		switch(operacao) {
+		case "somar": resultado = num1 + num2; break;
+		case "subtrair": resultado = num1 - num2; break;
+		case "multiplicar": resultado = num1 * num2; break;
+		case "dividir": 
+			if(num2 ==0) erro = "Divisão por zero não é permitido.";
+			else resultado = num1 / num2; break;
+		default: erro = "Operação inválida";
+		}
+		
+		Map<String, Object> resp = new HashMap<>();
+		resp.put("resultado: ", resultado);
+		resp.put("erro: ", erro);
+		return resp;
+		
+		
+		
+	}		
+    		
+    		
 }
